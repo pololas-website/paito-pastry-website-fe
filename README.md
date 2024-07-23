5c73d156378ee7e6da7861b764a39c227c73ca8a
veymarmc <46348503+veymarmc@users.noreply.github.com>
2024-07-23 10:56:50 -0400

    Init project (#1)
    * init project
    
    * set react development environment with webpack
    
    - install react and react-dom
    - install webpack babel and all dependencies like loaders and plugins
    - install webpack-dev-server to launch the server that will lauch the
      static files
    - deactivate the react scope rules in eslint
    - scope all the fiiles to check in the eslint
    - set babel.config with the present-env and preset-react with runtime
      automatic in order to not import React object in each file
    - add a singleQuote prettier rule
    - add inline-source-map in the devtool option for debugging
    - implement a mini app with React

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # paito-pastry-website-fe
+
+The resultant page will be [this](https://www.lolascupcakes.co.uk/SubMenu1/6/Cakes.htm)
---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { files: ['src/**/*.{js,mjs,cjs,jsx}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+
+  {
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];

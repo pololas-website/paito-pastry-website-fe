bdbc49c06a336c3b4e16079f1a27c0cf456e7644
veymarmc <veymar.montano@gmail.com>
2024-08-05 15:30:56 -0400

    apply react strict mode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
@@ -5,4 +6,8 @@ import App from './App';
 import '../public/css/base.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

da905104b531bf38fca5bdafd3ea5c0ae9af1dae
veymarmc <veymar.montano@gmail.com>
2024-12-07 08:53:38 -0400

    redirect to home when logged in from Auth pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+export { default as AuthPage } from './authentication/AuthPage';
 export { default as Cakes } from './cakes/Cakes';
 export { default as SignUp } from './authentication/SignUp';
 export { default as SignIn } from './authentication/SignIn';

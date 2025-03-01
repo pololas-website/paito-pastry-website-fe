6e9874bcd5e5b1bdb8829e9b25cd52bf1a44d02a
veymarmc <veymar.montano@gmail.com>
2025-03-01 18:00:14 -0400

    migrate the non-application-related files to ts

---
+++ src/utils/Observable.ts
@@ -0,0 +1,42 @@
+/*
+TODO:
+Apply safe typing with a real application
+commented at the mean time
+*/
+
+// class Observable {
+//   #value;
+//   #observers = new Set();
+
+//   constructor(value) {
+//     this.#value = value;
+//   }
+
+//   get observers() {
+//     return this.#observers;
+//   }
+
+//   get value() {
+//     return this.#value;
+//   }
+
+//   set value(newValue) {
+//     const previusValue = this.#value;
+//     this.#value = newValue;
+//     this.#observers.forEach((observer) => {
+//       observer(previusValue, newValue);
+//     });
+//   }
+
+//   addObservers(observers) {
+//     this.#observers = this.#observers.union(new Set(observers));
+//   }
+//   removeObserver(observer) {
+//     return this.#observers.delete(observer);
+//   }
+// }
+
+// export default Observable;
+
+const Observer = { test: 'test' };
+export default Observer;
---
+++ src/utils/throttle.ts
@@ -0,0 +1,53 @@
+/*
+TODO:
+Test these functions since the typing provided were not tested when migrated to Typescript.
+*/
+
+/**
+ * throttle a given function
+ * @param {Function} fn
+ * @param {number} delay in milliseconds.
+ * @returns the throttled function ready to be used
+ */
+export function throttle(fn: (...args: unknown[]) => void, delay: number) {
+  let timeout: NodeJS.Timeout | undefined;
+
+  return function (...args: unknown[]) {
+    if (!timeout) {
+      fn(...args);
+
+      timeout = setTimeout(() => {
+        timeout = undefined;
+      }, delay);
+    }
+  };
+}
+
+/*
+TODO:
+Test this function with a real application and find a way to type this
+for the moment will be commented.
+*/
+/**
+ * Throttle a function with the option to cancel it before it will execute.
+ * The function will be executed to the end of the delay parameter.
+ * @param {Function} fn function to throttle
+ * @param {number} delay in miliseconds
+ * @returns an object with the throttled function and the cancel function
+ */
+// export function throttleEndWithCancel(fn: (...args: unknown[]) => void, delay: number) {
+//   let timeout: NodeJS.Timeout | undefined;
+
+//   function throttled(...args: unknown[]) {
+//     if (!timeout) {
+//       timeout = setTimeout(() => {
+//         fn.apply(this, args);
+//         timeout = undefined;
+//       }, delay);
+//     }
+//   }
+
+//   const cancel = () => (timeout = clearTimeout(timeout) as undefined);
+
+//   return { throttled, cancel };
+// }

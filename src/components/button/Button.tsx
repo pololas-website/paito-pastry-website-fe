df772a341a3f2daf93cb617b27f00455355bcced
veymarmc <veymar.montano@gmail.com>
2025-03-04 19:13:42 -0400

    set forwardRef function to generic components

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,16 @@
 import styles from './button.module.css';
-import { PolymorphicComponent } from '../core/types';
+import { polymorphicForwardRef } from '../core/types';
 import {
   IBasicClasses,
   IStyles,
   getComposedClassName,
 } from '../core/HOC/baseElement/baseElement.utils';
 import { stringUtils } from '../../utils';
-
 interface IButtonProps extends IBasicClasses {
   icon?: boolean;
   rounded?: boolean;
+  className?: string;
+  children: React.ReactNode;
 }
 
 // TODO:
@@ -21,39 +22,45 @@ interface IButtonProps extends IBasicClasses {
 // in the types.
 // Goal, try to use a generic Button component with a HOC.
 // At the mean time we are going to specify the components manually in the Button but not using a HOC
-function Button<C extends React.ElementType = 'button'>({
-  icon,
-  rounded,
-  small,
-  primary,
-  contrast,
-  as,
-  children,
-  className,
-  ...rest
-}: PolymorphicComponent<C, IButtonProps>) {
-  const Container = as ?? 'button';
-  const composedClassName = getComposedClassName(
+
+const Button = polymorphicForwardRef<'button', IButtonProps>(
+  (
     {
+      icon,
+      rounded,
       small,
       primary,
       contrast,
+      as,
+      children,
+      className,
+      ...rest
     },
-    styles as unknown as IStyles,
-    className,
-  );
-  const classNames = stringUtils.join([
-    styles.btn,
-    rounded ? styles.rounded : '',
-    icon ? styles.icon : '',
-    composedClassName,
-  ]);
+    ref,
+  ) => {
+    const Container = as ?? 'button';
+    const composedClassName = getComposedClassName(
+      {
+        small,
+        primary,
+        contrast,
+      },
+      styles as unknown as IStyles,
+      className,
+    );
+    const classNames = stringUtils.join([
+      styles.btn,
+      rounded ? styles.rounded : '',
+      icon ? styles.icon : '',
+      composedClassName,
+    ]);
 
-  return (
-    <Container className={classNames} {...rest}>
-      {children}
-    </Container>
-  );
-}
+    return (
+      <Container {...rest} ref={ref} className={classNames}>
+        {children}
+      </Container>
+    );
+  },
+);
 
 export default Button;
--- src/components/core/types/index.ts
+++ src/components/core/types/index.ts
@@ -1,19 +1,57 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Empty Object type
-const emptySymbol = Symbol('EmptyObject type');
-export type EmptyObject = { [emptySymbol]?: never };
+// const emptySymbol = Symbol('EmptyObject type');
+// export type EmptyObject = { [emptySymbol]?: never };
 
-type AsProp<C extends React.ElementType> = {
-  as?: C;
-};
+import {
+  forwardRef,
+  type ComponentPropsWithRef,
+  type ElementType,
+  type ForwardRefExoticComponent,
+  type ForwardRefRenderFunction,
+  type ReactElement,
+} from 'react';
 
-export type PolymorphicComponent<
-  C extends React.ElementType,
-  CustomProps = EmptyObject,
-> = React.PropsWithChildren<AsProp<C> & CustomProps> &
-  Omit<React.ComponentPropsWithoutRef<C>, keyof CustomProps>;
-/*
-  TODO:
-    Currently we are using the React.ComponentPropsWithoutRef<T> built-in props
-    The task is to adap it to use the version with the Ref property which is
-    React.ComponentPropsWithRef<T>
-   */
+type DistributiveOmit<T, K extends keyof any> = T extends any
+  ? Omit<T, K>
+  : never;
+
+type Merge<A, B> = Omit<A, keyof B> & B;
+
+type DistributiveMerge<A, B> = DistributiveOmit<A, keyof B> & B;
+
+export type AsProps<
+  Component extends ElementType,
+  CustomProps extends object,
+  NativeProps extends object,
+> = DistributiveMerge<NativeProps, CustomProps & { as?: Component }>;
+
+export type PolymorphicWithRef<
+  Default extends OnlyAs,
+  Props extends object = {},
+  OnlyAs extends ElementType = ElementType,
+> = <T extends OnlyAs = Default>(
+  props: AsProps<T, Props, ComponentPropsWithRef<T>>,
+) => ReactElement | null;
+
+export type PolyForwardComponent<
+  Default extends OnlyAs,
+  Props extends object = {},
+  OnlyAs extends ElementType = ElementType,
+> = Merge<
+  ForwardRefExoticComponent<
+    Merge<ComponentPropsWithRef<Default>, Props & { as?: Default }>
+  >,
+  PolymorphicWithRef<Default, Props, OnlyAs>
+>;
+
+export type PolyRefFunction = <
+  Default extends OnlyAs,
+  Props extends object = {},
+  OnlyAs extends ElementType = ElementType,
+>(
+  Component: ForwardRefRenderFunction<any, Props & { as?: OnlyAs }>,
+) => PolyForwardComponent<Default, Props, OnlyAs>;
+
+export const polymorphicForwardRef = forwardRef as PolyRefFunction;

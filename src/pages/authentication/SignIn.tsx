4dfbd57738b1062f4ff03e720dea036827e8bf2b
veymarmc <veymar.montano@gmail.com>
2025-03-05 15:45:52 -0400

    make a basic integration of react-router and react-hook-form

--- src/pages/authentication/SignIn.tsx
+++ src/pages/authentication/SignIn.tsx
@@ -1,14 +1,26 @@
-import { Form, Link, redirect, useNavigate } from 'react-router-dom';
+import { Link, redirect, useNavigate, useSubmit } from 'react-router-dom';
 import { Button, Divider, Input } from '../../components';
 import { logInWithEmailAndPassword, signInWithGoogle } from '../../firebase';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import authStyles from './authentication.module.css';
 import styles from './signIn.module.css';
-
-// Initial commit for implement the application form validation
+interface IFormData {
+  email: string;
+  password: string;
+}
 
 export default function SignIn() {
   const navigate = useNavigate();
+  const submit = useSubmit();
+  const { register, handleSubmit } = useForm<IFormData>();
+
+  const onSubmit: SubmitHandler<IFormData> = (data) => {
+    const formData = new FormData();
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+    submit(formData, { method: 'POST' });
+  };
 
   const handleOnSignInWithGoogle = async () => {
     await signInWithGoogle();
@@ -17,14 +29,27 @@ export default function SignIn() {
 
   return (
     <section className={`container ${authStyles['auth-container']}`}>
-      <Form method="post" className={authStyles.form}>
+      <form className={authStyles.form} onSubmit={handleSubmit(onSubmit)}>
         <h4 className={`heading-4 ${authStyles.title}`}>Welcome back!</h4>
-        <Input type="email" name="email" placeholder="Email Address" required />
+        {/* TODO:
+          Currently there's a warning in the console about: Function components cannot be given refs.
+          This is due to when applying a special HOOK to the forwarded ref input component, The HOC is returning          
+          a parameter component. The solution is that the HOC should return another forward component to
+          delete this warning.
+          Currently the typings are complex.
+          Apply the proposed solution with the right typing.
+          Components to test at this time: Input.
+          HOC to test: withBaseInlineElement.
+        */}
+        <Input
+          type="email"
+          placeholder="Email Address"
+          {...register('email', { required: true })}
+        />
         <Input
           type="password"
-          name="password"
           placeholder="Password"
-          required
+          {...register('password', { required: true })}
         />
         <Button type="submit" className={styles['signin-button']}>
           Sign In
@@ -60,7 +85,7 @@ export default function SignIn() {
             </Button>
           </div>
         </div>
-      </Form>
+      </form>
     </section>
   );
 }
